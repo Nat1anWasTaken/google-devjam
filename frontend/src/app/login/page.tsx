ed67032a96c9bff804ce67017fc74ba44cd4b4e2
Nat1anWasTaken <nathan@nat1an.xyz>
2025-06-07 14:22:31 +0800

    feat(frontend/src/app/login/page.tsx, frontend/src/app/register/page.tsx): add navigation to home on successful login/registration

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,12 +3,16 @@
 import { LoginData, LoginForm } from "@/components/login-form";
 import { loginUser } from "@/lib/api";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
+
   const mutation = useMutation({
     mutationFn: loginUser,
     onSuccess: (data) => {
       console.log("Login successful:", data);
+      router.push("/");
     },
     onError: (error) => {
       console.error("Login failed:", error);
--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -3,12 +3,16 @@
 import { RegisterData, RegisterForm } from "@/components/register-form";
 import { registerUser } from "@/lib/api";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
+  const router = useRouter();
+
   const mutation = useMutation({
     mutationFn: registerUser,
     onSuccess: (data) => {
       console.log("Registration successful:", data);
+      router.push("/");
     },
     onError: (error) => {
       console.error("Registration failed:", error);
